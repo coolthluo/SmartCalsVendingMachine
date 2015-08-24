8243c652a82b3cb4dbfbf9ee71ae6f45d727c578
coolthluo <zhengbin.luo@gmail.com>
2015-08-24 16:11:51 -0700

    uuu
    jj

--- CreateTables.sql
+++ CreateTables.sql
@@ -34,6 +34,10 @@ FOREIGN KEY (machineid) REFERENCES machines(ID) ON DELETE CASCADE,
 FOREIGN KEY (itemid) REFERENCES items(ID) ON DELETE CASCADE
 );
 
+
+
+
+
 CREATE TABLE sales (
 ID INT NOT NULL AUTO_INCREMENT,
 machineid INT,
@@ -46,9 +50,9 @@ FOREIGN KEY (itemid) REFERENCES items(ID) ON DELETE CASCADE
 );
 
 CREATE TABLE employees (
-ID INT NOT NULL PRIMARY KEY,
+ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 name VARCHAR(30),
-password VARCHAR(15),
+password VARCHAR(255),
 isManager INT
 );
 
@@ -72,3 +76,44 @@ insert into items values (11, "Chocolate", 1, "snack", 155, 14, "itemsinfo/item_
 insert into employees values (1, "Sam", "one", 1);
 insert into employees values (2, "Tom", "two", 0);
 
+insert into machines values(1, "San francisco","2015-05-30 00:00:00", "2015-07-30 00:00:00");
+insert into machines values(2, "San diego","2015-05-31 00:00:00", "2015-08-30 00:00:00");
+insert into machines values(3, "Santa clara","2015-06-01 00:00:00", "2015-06-30 00:00:00");
+insert into machines values(4, "San jose","2015-04-25 00:00:00", "2015-07-30 00:00:00");
+
+
+insert into machine_item values(1, 1, 100, 50);
+insert into machine_item values(1, 2, 100, 80);
+insert into machine_item values(1, 3, 100, 0);
+insert into machine_item values(1, 4, 100, 11);
+insert into machine_item values(1, 5, 100, 23);
+insert into machine_item values(1, 6, 100, 50);
+insert into machine_item values(1, 7, 100, 80);
+insert into machine_item values(1, 8, 100, 0);
+insert into machine_item values(1, 9, 100, 11);
+insert into machine_item values(1, 10, 100, 23);
+insert into machine_item values(1, 11, 100, 23);
+
+insert into machine_item values(2, 1, 100, 80);
+insert into machine_item values(2, 2, 100, 80);
+insert into machine_item values(2, 3, 100, 0);
+insert into machine_item values(2, 4, 100, 70);
+insert into machine_item values(2, 5, 100, 23);
+insert into machine_item values(2, 6, 100, 70);
+insert into machine_item values(2, 7, 100, 70);
+insert into machine_item values(2, 8, 100, 9);
+insert into machine_item values(2, 9, 100, 11);
+insert into machine_item values(2, 10, 100, 70);
+insert into machine_item values(2, 11, 100, 23);
+
+insert into machine_item values(3, 1, 100, 5);
+insert into machine_item values(3, 2, 100, 60);
+insert into machine_item values(3, 3, 100, 50);
+insert into machine_item values(3, 4, 100, 90);
+insert into machine_item values(3, 5, 100, 0);
+insert into machine_item values(3, 6, 100, 5);
+insert into machine_item values(3, 7, 100, 60);
+insert into machine_item values(3, 8, 100, 50);
+insert into machine_item values(3, 9, 100, 90);
+insert into machine_item values(3, 10, 100, 34);
+insert into machine_item values(3, 11, 100, 56);
